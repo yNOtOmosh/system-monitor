d0f5883f96845e8ab8faffb267f2dcf4bdac0391
yNOtOmosh <onyango.omondi.tony@gmail.com>
2025-10-07 22:31:30 +0300

    in the main.rs, we set up axum- based websocket server to broadcast live system metricsevery second. with an integretade tokio runtime for async handling and periodic metric collection. there is a broadcast channel for multicasting json data to all connected clients which serves as the main entry point for the real-time system monitor backend.

--- sys-mon-dash/src/main.rs
+++ sys-mon-dash/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use tokio::sync::broadcast;
+
+mod websocket;
+mod metrics;
+
+async fn main() {
+    let (tx, _) = broadcast::channel::<string>(16);
+
+    let app = websocket::app(tx.clone());
+
+    tokio::spawn(websocket::start_broadcaster(tx));
+
+    println!("Server running on http://127.0.0.1:3000"
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
